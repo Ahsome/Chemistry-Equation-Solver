61e7c6e365e00553a587cc2cb6cf04aab4c55529
Ahkam Nihardeen <ahkamyaqeen@outlook.com>
2019-12-25 20:00:34 +1000

    Complete Rewrite
    Done a complete rewrite of the old project. Old code still exists, will remove

---
+++ Equation.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChemistryEquationSolver
+{
+    class Equation
+    {
+        public Equation(string equationString)
+        {
+            AddReactants(equationString);
+            AddProducts(equationString);
+            AddReactantElements();
+            AddProductElements();
+        }
+
+        private void AddProductElements()
+        {
+            AddElements(Products, ProductsElements);
+        }
+
+        private void AddReactantElements()
+        {
+            AddElements(Reactants, ReactantElements);
+        }
+
+        private void AddElements(Dictionary<string, int> chemicals, Dictionary<string, int> elementProperty)
+        {
+            foreach (var chemical in chemicals.Keys)
+            {
+                StringBuilder elements = new StringBuilder();
+                foreach (char c in chemical)
+                {
+                    if (Char.IsUpper(c) && elements.Length > 0)
+                    {
+                        elements.Append(' ');
+                    };
+                    elements.Append(c);
+                }
+
+                foreach (var elementWithNum in elements.ToString().Split(' '))
+                {
+                    string element = "";
+                    string number = "";
+                    foreach (var c in elementWithNum.ToString())
+                    {
+                        if (Char.IsLetter(c))
+                        {
+                            element += c;
+                        }
+                        else if (Char.IsNumber(c))
+                        {
+                            number += c;
+                        }
+                    }
+
+                    int value;
+                    chemicals.TryGetValue(chemical, out value);
+
+                    if (!number.Equals(""))
+                    {
+                        value *= int.Parse(number);
+                    }
+
+                    AddToProperty(value, element, elementProperty);
+                }
+            }
+        }
+
+        private string[] ExtractProducts(string equationString)
+        {
+            return equationString.Split('=')[1].Split('+');
+        }
+
+        private string[] ExtractReactants(string equationString)
+        {
+            return equationString.Split('=')[0].Split('+');
+        }
+
+        public Dictionary<string, int> Reactants { get; private set; } = new Dictionary<string, int>();
+        public Dictionary<string, int> Products { get; private set; } = new Dictionary<string, int>();
+        public Dictionary<string, int> ReactantElements { get; private set; } = new Dictionary<string, int>();
+        public Dictionary<string, int> ProductsElements { get; private set; } = new Dictionary<string, int>();
+        private void AddReactants(string equationString)
+        {
+            string[] reactants = ExtractReactants(equationString);
+            foreach (var reactant in reactants)
+            {
+                int coefficient = GetCoefficient(reactant);
+                string reactantNoCoefficient = reactant.Substring(coefficient.ToString().Length);
+                AddToProperty(coefficient, reactantNoCoefficient, Reactants);
+            }
+        }
+
+        private int GetCoefficient(string reactant)
+        {
+            string coefficient = "";
+            foreach (var character in reactant)
+            {
+                if (Char.IsDigit(character))
+                {
+                    coefficient += character;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+            if (!coefficient.Equals(""))
+            {
+                return int.Parse(coefficient);
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        private void AddProducts(string equationString)
+        {
+            string[] products = ExtractProducts(equationString);
+            foreach (var product in products)
+            {
+                int coefficient = GetCoefficient(product);
+                string productNoCoefficient = product;
+                if (coefficient != 1)
+                {
+                    productNoCoefficient = product.Substring(coefficient.ToString().Length);
+                }
+
+                AddToProperty(coefficient, productNoCoefficient, Products);
+            }
+        }
+
+        private void AddToProperty(int value, string key, Dictionary<string, int> property)
+        {
+            if (property.TryGetValue(key, out int previousValue))
+            {
+                property[key] = previousValue + value;
+            }
+            else
+            {
+                property.Add(key, value);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,16 +8,14 @@ namespace ChemistryEquationSolver
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
 
             Console.WriteLine("What equation would you like to check/balance?");
-            string equation = Console.ReadLine().Replace(" ", String.Empty);
-            string[] equationArray = equation.Split('=');
-            string[] reactants = equationArray[0].Split('+');
-            string[] products = equationArray[1].Split('+');
+            var Equation = new Equation(Console.ReadLine());
 
             Console.Clear();
+         
             while (true)
             {
                 Console.WriteLine("What would you like to do?\n - CHECK\n - BALANCE (NOT IMPLEMENTED)");
@@ -25,22 +23,50 @@ namespace ChemistryEquationSolver
                 if (Console.ReadLine() == "CHECK")
                 {
                     Console.Clear();
-                    int[] reactantNumbers = ElementNumbers(reactants);
-                    int[] productNumbers = ElementNumbers(products);
-
-                    if (reactantNumbers.Where((t, i) => t != productNumbers[i]).Any())
+                    if (IsEqual(Equation.ReactantElements, Equation.ProductsElements))
                     {
-                        Console.WriteLine("Not balanced, dufus");
+                        Console.WriteLine("HELL YEAH, BALANCING!");
+                        
                     }
 
                     else
                     {
-                        Console.WriteLine("HELL YEAH, BALANCING!");
+                        Console.WriteLine("Not balanced, dufus");
                     }
                     Console.ReadKey(true);
                     break;
                 }
             }
+         
+        }
+
+        private static bool IsEqual(Dictionary<string, int> dict, Dictionary<string, int> dict2)
+        {
+            bool equal = false;
+            if (dict.Count == dict2.Count) // Require equal count.
+            {
+                equal = true;
+                foreach (var pair in dict)
+                {
+                    int value;
+                    if (dict2.TryGetValue(pair.Key, out value))
+                    {
+                        // Require value be equal.
+                        if (value != pair.Value)
+                        {
+                            equal = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        // Require key be present.
+                        equal = false;
+                        break;
+                    }
+                }
+            }
+            return equal;
         }
 
         private static int[] ElementNumbers(string[] substances)
